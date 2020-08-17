8347fa743b56cf79e31af792d4af173437d7f090
DM TRUM <minhvuong010700@gmail.com>
2020-08-17 20:45:57 +0700

    vuong push ne

--- TeamCodeDao.cpp
+++ TeamCodeDao.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 using namespace std;
-
+void lietKeNguyenDuong(int n)
+{
+	for(int i=1;i<=n;++i)
+	{
+		if(i%2==0)
+		cout<<i<<" ";
+	}
+}
+int sumUocNguyenDuongN(int n)
+{
+	for(int i=1;i<=n;++i)
+	{
+		if(n%i==0)
+		
+	}
+}
 int main()
 {
+	int n;
+	cout<<"Nhap N: ";
+	cin>>n;
+	cout<<"Cac so nguyen duong nho hon "<<n<<" :";
+	lietKeNguyenDuong(n);
 	return 0;
 }
